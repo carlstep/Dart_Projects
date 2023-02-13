a8e6b96e71aaedc3b1de26645fcbb9761cbf9278
carlstep <carlstep333@me.com>
2023-02-13 13:05:59 +0700

    function types

--- 03_Functions/functionswork.dart
+++ 03_Functions/functionswork.dart
@@ -8,6 +8,8 @@ void printMyName(String fistName, String lastName) {
   print('$fistName $lastName');
 }
 
+// functionName(int functionParameterOne, String functionParameterTwo)
+// numOne & numTwo are known as parameters
 void addTwoNumbers(int numOne, int numTwo) {
   var total = numOne + numTwo;
   print('the sum is... $total');
@@ -16,6 +18,13 @@ void addTwoNumbers(int numOne, int numTwo) {
 void findCube(num numberValue) {
   num cube = numberValue * numberValue * numberValue;
   print('the cube of $numberValue is $cube');
+}
+
+void convertA(
+    double currencyAValue, double currencyBRate, double currencyCRate) {
+  double conversionB = currencyAValue * currencyBRate;
+  double conversionC = currencyAValue * currencyCRate;
+  print('$currencyAValue KRH is... \n$conversionB USD \n$conversionC THB');
 }
 
 void main() {
@@ -27,9 +36,19 @@ void main() {
 
   printName();
   printMyName(firstName, secondName);
+
+  // 45, 94 are known as arguments. The argument values are passed to the function.
   addTwoNumbers(45, 94);
 
   print('Enter a number....');
   num? number = num.parse(stdin.readLineSync()!);
   findCube(number);
+
+  print('enter an amount...');
+  double? inputAmount = double.parse(stdin.readLineSync()!);
+  double currencyAValueX = inputAmount;
+  double currencyBRateX = 0.00025;
+  double currencyCRateX = 0.0034;
+
+  convertA(currencyAValueX, currencyBRateX, currencyCRateX);
 }
---
+++ 03_Functions/functiontypes.dart
@@ -0,0 +1,66 @@
+// - No Parameter AND No Return Type
+// - uses the void keyword = no Return Type
+// - empty () implies there are no parameters required by the function
+void printInfo() {
+  print('My name is John Doe.');
+}
+
+// Parameter AND No Return Type
+void addNumbers(int num1, int num2) {
+  print('the sum of $num1 and $num2 is ${num1 + num2}');
+}
+
+// No Parameter AND Return Type
+String selectedCountry() {
+  return 'Cambodia';
+}
+
+int visaDays() {
+  return 30;
+}
+
+// Parameter AND Return Type - #1
+String fullName(String name1, String name2) {
+  String name = name1 + ' ' + name2;
+  return name;
+}
+
+// Parameter AND Return Type - #2
+double convertCurrency(double amount, double rateA) {
+  return amount * rateA;
+}
+
+void main() {
+// - No Parameter AND No Return Type
+// - no arguments being passed to the function.
+  printInfo();
+
+// Parameter AND No Return Type
+// inside the () are the arguments, passed to the function. the function has parameters.
+  addNumbers(10, 32);
+
+// No Parameter AND Return Type - #1
+  String country = selectedCountry(); // return type is String
+  print(country);
+  int visa = visaDays(); // return type is int
+  print(visa);
+
+// No Parameter AND Return Type - #2
+  int daysOfStay = 22;
+  if (daysOfStay >= visaDays()) {
+    print('you have overstayed by ${daysOfStay - visa}');
+  } else {
+    print('you have ${visa - daysOfStay} days remaining');
+  } // return type is int
+
+// Parameter AND Return Type - #1
+  String firstName = 'peter';
+  String familyName = 'jones';
+
+  String correctName = fullName(firstName, familyName);
+  print(correctName);
+
+// Parameter AND Return Type - #2
+  double conversion = convertCurrency(56000, 0.000248);
+  print('the KHR amount in USD is $conversion');
+}
