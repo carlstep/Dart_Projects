0f8752ecd762e1b29d9a85a00a8fee249aeb1067
carlstep <carlstep333@me.com>
2023-02-13 20:56:46 +0700

    local vs global scope

---
+++ 03_Functions/scope.dart
@@ -0,0 +1,14 @@
+// example of - global scope
+String globalText = 'text outside main(), is global. can be access elsewhere.';
+double pi = 3.14;
+
+void main() {
+  // example of - local/method scope
+  // can be used inside the method block, but no outside
+  String methodText = 'text inside the main function. can not be accessed.';
+  int methodNumber = 42;
+  print(methodText);
+  print('the number in the method is $methodNumber');
+  print(globalText);
+  print(pi);
+}
