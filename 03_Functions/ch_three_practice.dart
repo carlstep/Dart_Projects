e9a2101a54e34989c54a6371fd84de4f7e48dc76
carlstep <carlstep333@me.com>
2023-02-16 12:47:32 +0700

    not complete

---
+++ 03_Functions/ch_three_practice.dart
@@ -0,0 +1,14 @@
+// #1 write a program to print own name using a function
+void printName(String firstName, String familyName) {
+  print('$firstName $familyName');
+}
+
+// #2 write a program to print even numbers between intervals using function
+void printEvenNumbers(int intervalNum, int maxNum) {}
+
+void main() {
+// #1 write a program to print own name using a function
+  printName('carl', 'jones');
+
+// #2 write a program to print even numbers between intervals using function
+}
